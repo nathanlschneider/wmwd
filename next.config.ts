8d6814fd6b0d614609fb5a1b48ed05f0741f0624
Nathan Schneider <nate@qwerkly.com>
2025-07-11 16:28:06 -0400

    changes

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
         source: "/logger/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3002" }, // MUST be explicit if credentials are true
+          { key: "Access-Control-Allow-Origin", value: "https://localhost:3002" }, // MUST be explicit if credentials are true
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ],
